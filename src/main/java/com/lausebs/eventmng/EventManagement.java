3d7e1aa13b39c512b1e402351741ba219585fb50
DSebs <sebastian.diaz@estudiantesunibague.edu.co>
2024-08-18 11:23:57 -0500

    First Commit

---
+++ src/main/java/com/lausebs/eventmng/EventManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lausebs.eventmng;
+
+/**
+ *
+ * @author estigia
+ */
+public class EventManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
