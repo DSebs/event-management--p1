918570e4531648f50b9a094a73fb63b5b02b63e8
DSebs <sebastian.diaz@estudiantesunibague.edu.co>
2024-08-21 16:56:45 -0500

    Add nav bar

--- src/main/java/com/lausebs/eventmng/EventManagement.java
+++ src/main/java/com/lausebs/eventmng/EventManagement.java
@@ -4,6 +4,8 @@
 
 package com.lausebs.eventmng;
 
+import com.lausebs.eventmng.view.FramePrincipal;
+
 /**
  *
  * @author estigia
@@ -11,6 +13,7 @@ package com.lausebs.eventmng;
 public class EventManagement {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FramePrincipal framePrinciopal = new FramePrincipal();
+        framePrinciopal.setVisible(true);
     }
 }
